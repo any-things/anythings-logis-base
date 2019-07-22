76ddfb2d4401f88eb9dc7505e8700b78ee99aad0
hatiolab <shortstop@hatiolab.com>
2019-07-22 16:26:37 +0900

    ...

--- src/main/java/xyz/anythings/base/service/impl/AbstractLogisService.java
+++ src/main/java/xyz/anythings/base/service/impl/AbstractLogisService.java
@@ -2,14 +2,14 @@ package xyz.anythings.base.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import xyz.anythings.sys.service.AbstractQueryService;
+import xyz.anythings.sys.service.AbstractExecutionService;
 
 /**
  * 물류 최상위 서비스
  *  
  * @author shortstop
  */
-public class AbstractLogisService extends AbstractQueryService {
+public class AbstractLogisService extends AbstractExecutionService {
 
 	/**
 	 * 물류 서비스 파인더
