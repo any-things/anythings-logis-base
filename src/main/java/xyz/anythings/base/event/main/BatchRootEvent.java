aed1e5555ee6192ac0cc91ec7c1935a9f623b412
wryang <yang@yang-mac.local>
2019-10-08 11:56:49 +0900

    모듈 래빗 큐 설정 추가
    1

--- src/main/java/xyz/anythings/base/event/main/BatchRootEvent.java
+++ src/main/java/xyz/anythings/base/event/main/BatchRootEvent.java
@@ -3,6 +3,7 @@ package xyz.anythings.base.event.main;
 import java.util.Map;
 
 import xyz.anythings.sys.event.model.SysEvent;
+import xyz.elidom.util.ValueUtil;
 
 /**
  * 작업 관련 Root 이벤트 
@@ -46,15 +47,36 @@ config	설정 메타 데이터(json) : { 'type' : 'proc' , 'procedure' : 'sp_bat
 	
 	/**
 	 * 1 . BEFORE 
-	 * 2 . MAIN
-	 * 3 . AFTER
+	 * 2 . AFTER
 	 */
 	protected short eventStep;
+	
+	
+	/**
+	 * DAS / DPS / SMS / ...... 
+	 */
+	protected String bizType;
+	
+	/**
+	 * 작업 유형 
+	 * DAS : DAS / RTN .....
+	 * DPS : DPS / QPS .....
+	 * SMS : .......
+	 * .....
+	 */
+	protected String jobType;
+	
+	/**
+	 * 다음 이벤트를 계속 발생 할건지 flag
+	 * DEFAULT = false
+	 */
+	protected boolean isAfterEventCancel;
 	
 	
 	public BatchRootEvent(short eventStep) {
 		super();
 		this.setEventStep(eventStep);
+		this.setAfterEventCancel(false);
 	}
 	
 	
@@ -85,4 +107,24 @@ config	설정 메타 데이터(json) : { 'type' : 'proc' , 'procedure' : 'sp_bat
 	public void setProcedureParams(Map<String, ?> procedureParams) {
 		this.procedureParams = procedureParams;
 	}
+
+
+	public boolean isAfterEventCancel() {
+		return isAfterEventCancel;
+	}
+
+
+	public void setAfterEventCancel(boolean isAfterEventCancel) {
+		this.isAfterEventCancel = isAfterEventCancel;
+	}
+	
+	/**
+	 * 배치별 셋팅값 불러오기 
+	 * 설비, 화주 , 호기 stack 구조 값 
+	 * @return
+	 */
+	public Map<String,Object> getBatchSetting() {
+		return ValueUtil.newMap("", "");
+	}
+	
 }
