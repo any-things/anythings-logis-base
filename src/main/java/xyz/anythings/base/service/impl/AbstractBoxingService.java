fed5074e060c8238fa65ed0a55a67d5a851367f7
wryang <yang@yang-mac.local>
2019-11-07 14:28:53 +0900

    boxing 서비스  재 커밋

--- src/main/java/xyz/anythings/base/service/impl/AbstractBoxingService.java
+++ src/main/java/xyz/anythings/base/service/impl/AbstractBoxingService.java
@@ -1,13 +1,14 @@
 package xyz.anythings.base.service.impl;
 
 import xyz.anythings.base.service.api.IBoxingService;
+import xyz.anythings.sys.service.AbstractExecutionService;
 
 /**
  * 박스 처리 서비스 기본 구현  
  * @author yang
  *
  */
-public abstract class AbstractBoxingService implements IBoxingService {
+public abstract class AbstractBoxingService extends AbstractExecutionService implements IBoxingService {
 	
 
 }
