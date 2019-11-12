01e6b9ae6249374416f76fe4ce48166fa339ee56
hatiolab <shortstop@hatiolab.com>
2019-11-12 22:48:07 +0900

    스테이지 범위 내 설정 API 작업 중

--- src/main/java/xyz/anythings/base/service/util/StageIndConfigUtil.java
+++ src/main/java/xyz/anythings/base/service/util/StageIndConfigUtil.java
@@ -37,4 +37,327 @@ package xyz.anythings.base.service.util;
  */
 public class StageIndConfigUtil {
 
+	/**
+	 * 표시기가 점등되기 전 지연되는 시간입니다. (100ms 단위)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static int getIndOnDelayTime(String stageCd, String indType) {
+		// ind.action.delay.before.on				
+		return 0;
+	}
+
+	/**
+	 * 표시기의 취소 버튼을 눌렀을 때 표시기가 소등되기까지의 지연 시간입니다. (100ms)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static int getIndOnDelayTimeCancelPushed(String stageCd, String indType) {
+		// ind.action.delay.cancel.button.off
+		return 0;
+	}
+
+	/**
+	 * 표시기가 이미 소등된 상태에서 소등 요청을 받았을 때 응답 메시지를 보낼 지 여부입니다.
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static boolean isNoackWhenAlreadyOffEnabled(String stageCd, String indType) {
+		// ind.action.send.off.ack.already.off
+		return false;
+	}
+	
+	/**
+	 * 다음 작업을 점등하기 전에 표시될 문자열입니다.
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getDisplayStringBeforeIndOn(String stageCd, String indType) {
+		// ind.action.show.string.before.on
+		return null;
+	}
+
+	/**
+	 * 점등 전에 문자를 표시할 시간입니다. (100ms 단위)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static int getDisplayIntervalBeforeIndOn(String stageCd, String indType) {
+		// ind.action.show.string.delay.before.on	
+		return 0;
+	}
+
+	/**
+	 * 표시기의 상태 보고 주기입니다. (초 단위)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static int getIndStateReportInterval(String stageCd, String indType) {
+		// ind.action.status.report.interval
+		return 0;
+	}
+
+	/**
+	 * 표시기 버튼 색상의 로테이션 순서입니다.
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String[] getIndButtonColorForRotation(String stageCd, String indType) {
+		// ind.job.color.rotation.seq
+		return null;
+	}
+	
+	/**
+	 * 재고 실사 작업에서 표시기 버튼의 기본 색상입니다.
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getIndColorForStocktaking(String stageCd, String indType) {
+		// ind.job.color.stocktaking				
+		return null;
+	}
+	
+	/**
+	 * 세그먼트 사용 개수
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static int getSegmentCount(String stageCd, String indType) {
+		return getSegmentRoles(stageCd, indType).length;
+	}
+	
+	/**
+	 * 작업 점등 시 각 세그먼트가 나타낼 정보입니다 - 첫번째/두번째/세번째 세그먼트 역할 -> R(릴레이 순서)/B(Box)/P(PCS)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String[] getSegmentRoles(String stageCd, String indType) {
+		// ind.job.segment.roles.on								
+		return null;
+	}
+	
+	/**
+	 * 표시 세그먼트의 첫 번째 숫자와 매핑되는 역할 (T: 총 수량, R: 총 남은 수량, F: 총 처리한 수량, P: 방금 전 처리한 낱개 수량, B: 방금 전 처리한 박스 수량)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getSegment1RoleForDisplay(String stageCd, String indType) {
+		// ind.show.segment1.mapping.role
+		return null;
+	}
+
+	/**
+	 * 표시 세그먼트의 두 번째 숫자와 매핑되는 역할 (T: 총 수량, R: 총 남은 수량, F: 총 처리한 수량, P: 방금 전 처리한 낱개 수량, B: 방금 전 처리한 박스 수량)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getSegment2RoleForDisplay(String stageCd, String indType) {
+		// ind.show.segment2.mapping.role
+		return null;
+	}
+
+	/**
+	 * 표시 세그먼트의 세 번째 숫자와 매핑되는 역할 (T: 총 수량, R: 총 남은 수량, F: 총 처리한 수량, P: 방금 전 처리한 낱개 수량, B: 방금 전 처리한 박스 수량)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getSegment3RoleForDisplay(String stageCd, String indType) {
+		// ind.show.segment3.mapping.role
+		return null;
+	}
+	
+	/**
+	 * 최대 릴레이 번호
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getMaxRelayNo(String stageCd, String indType) {
+		// ind.show.relay.max.no
+		return null;
+	}
+	
+	/**
+	 * 표시기의 버튼이 깜빡이는 주기입니다. (100ms 단위)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getButtonBlinkInterval(String stageCd, String indType) {
+		// ind.show.button.blink.interval			
+		return null;
+	}
+	
+	/**
+	 * 표시기의 버튼이 점등되는 방식입니다. (B: 깜빡임, S: 항상 켜짐)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getButtonOnMode(String stageCd, String indType) {
+		// ind.show.button.on.mode					
+		return null;
+	}
+	
+	/**
+	 * 표시기 Full Box 터치시 버튼 깜빡임 여부
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static boolean isFullButtonBlink(String stageCd, String indType) {
+		// ind.show.fullbox.button.blink
+		return false;
+	}
+	
+	/**
+	 * 표시기 자체적으로 표시 형식을 변경하는 모드
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getShowViewType(String stageCd, String indType) {
+		// ind.show.view-type
+		return null;
+	}
+	
+	/**
+	 * 표시기 숫자의 정렬 방향입니다. (R / L)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getShowNumberAlignment(String stageCd, String indType) {
+		// ind.show.number.alignment
+		return null;
+	}
+	
+	/**
+	 * LED 바가 깜박이는 주기입니다. (100ms 단위)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static int getLedBlinkInterval(String stageCd, String indType) {
+		// ind.led.blink.interval
+		return 0;
+	}
+
+	/**
+	 * LED 바의 밝기입니다. (1~10)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static int getLedBrightness(String stageCd, String indType) {
+		// ind.led.brightness						
+		return 0;
+	}
+	
+	/**
+	 * LED 바를 깜빡이게 할 지 여부
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static boolean isLedBlink(String stageCd, String indType) {
+		// ind.show.fullbox.button.blink
+		return false;
+	}
+	
+	/**
+	 * LED 바를 사용할지 여부입니다.
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static boolean isUseLed(String stageCd, String indType) {
+		// ind.led.use.enabled						
+		return false;
+	}
+
+	/**
+	 * LED 바를 사용할 호기 리스트 (콤마로 구분)
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String[] getRackOfUsingLed(String stageCd, String indType) {
+		// ind.led.use.enabled.racks										
+		return null;
+	}
+		
+	/**
+	 * 표시기 버튼 사용 여부
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static boolean isUseButton(String stageCd, String indType) {
+		// ind.buttons.enable							
+		return false;
+	}
+	
+	/**
+	 * 표시기 최신 버전 정보 설정
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static boolean getIndLatestReleaseVersion(String stageCd, String indType) {
+		// ind.latest.release.version				
+		return false;
+	}
+	
+	/**
+	 * Gateway 최신 버전 정보 설정
+	 * 
+	 * @param stageCd
+	 * @param indType
+	 * @return
+	 */
+	public static String getGwLatestReleaseVersion(String stageCd, String indType) {
+		// ind.gw.latest.release.version					
+		return null;
+	}
+
 }
--- src/main/java/xyz/anythings/base/service/util/StageJobConfigUtil.java
+++ src/main/java/xyz/anythings/base/service/util/StageJobConfigUtil.java
@@ -13,6 +13,7 @@ package xyz.anythings.base.service.util;
  * - parent.system.if.boxtype.table				상위 시스템 박스 유형 I/F 테이블 명 
  * - parent.system.if.order.table				상위 시스템 주문 I/F 테이블 명
  * - parent.system.if.pick-result.table			상위 시스템 피킹 실적 I/F 테이블 명
+ * - parent.system.if.box-result.table			상위 시스템 박스 실적 I/F 테이블 명
  * - parent.system.if.totalpicking.table		상위 시스템 토털 피킹 I/F 테이블 명
  * - parent.system.if.company.procedure			상위 시스템 고객사 I/F 프로시져 명
  * - parent.system.if.sku.procedure				상위 시스템 SKU I/F 프로시져 명
@@ -59,6 +60,7 @@ package xyz.anythings.base.service.util;
  * - dps.indicator.type  						DPS 표시기 유형 (통신 프로토콜 기준)
  * - dps.order.delete.when.order_cancel			DPS 주문 취소시 데이터 삭제 여부
  * - dps.order.ordergroup.field					DPS 주문 그룹 필드로 사용할 주문 테이블의 필드명
+ *
  * - dps.batch.split-by-rack.enabled			DPS 배치 분리 여부
  * - dps.sku.popula.rank.calc.days				DPS SKU 물량 Rank 선정 기준 데이터 범위 (일자)
  * - dps.station.wait-pool.count				DPS 작업 스테이션에 대기할 박스 최대 개수 (-1이면 무한대)
@@ -68,4 +70,341 @@ package xyz.anythings.base.service.util;
  */
 public class StageJobConfigUtil {
 	
+	/**
+	 * 상위 시스템 인터페이스 유형 (datasource / dblink / if-table)
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfType(String stageCd) {
+		// parent.system.if.iftype					
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 I/F를 위한 DB Link 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfDbLinkName(String stageCd) {
+		// parent.system.if.dblink.name
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 데이터소스 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfDatasource(String stageCd) {
+		// parent.system.if.datasource.name
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 고객사 I/F 테이블 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfCompanyTable(String stageCd) {
+		// parent.system.if.company.table
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 SKU I/F 테이블 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfSkuTable(String stageCd) {
+		// parent.system.if.sku.table		
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 매장 I/F 테이블 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfShopTable(String stageCd) {
+		// parent.system.if.shop.table	
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 박스 유형 I/F 테이블 명 
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfBoxTypeTable(String stageCd) {
+		// parent.system.if.boxtype.table			
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 주문 I/F 테이블 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfOrderTable(String stageCd) {
+		// parent.system.if.order.table 
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 피킹 실적 I/F 테이블 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfPickingResultTable(String stageCd) {
+		// parent.system.if.pick-result.table
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 박스 실적 I/F 테이블 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfBoxResultTable(String stageCd) {
+		// parent.system.if.box-result.table
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 토털 피킹 I/F 테이블 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfTotalPickingTable(String stageCd) {
+		// parent.system.if.totalpicking.table
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 고객사 I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfCompanyProcedure(String stageCd) {
+		// parent.system.if.company.procedure
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 SKU I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfSkuProcedure(String stageCd) {
+		// parent.system.if.sku.procedure
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 매장 I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfShopProcedure(String stageCd) {
+		// parent.system.if.shop.procedure
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 박스 유형 I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfBoxTypeProcedure(String stageCd) {
+		// parent.system.if.boxtype.procedure
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 주문 I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfOrderProcedure(String stageCd) {
+		// parent.system.if.order.procedure
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 피킹 실적 I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfPickingResultProcedure(String stageCd) {
+		// parent.system.if.pick-result.procedure
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 박스 실적 I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfBoxResultProcedure(String stageCd) {
+		// parent.system.if.box-result.procedure
+		return null;
+	}
+	
+	/**
+	 * 상위 시스템 토털 피킹 I/F 프로시져 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getParentIfTotalPickingProcedure(String stageCd) {
+		// parent.system.if.totalpicking.procedure
+		return null;
+	}
+
+	/**
+	 * 설비 이벤트 무시할 지 여부
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static boolean isIgnoreEquipStatusReport(String stageCd) {
+		// cmm.equip.status.report.ignore.flag
+		return false;
+	}
+
+	/**
+	 * 미들웨어 접수 메시지를 로깅할 지 여부
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String isLogMwMessage(String stageCd) {
+		// cmm.receive.logging.enabled
+		return null;
+	}
+	
+	/**
+	 * 재고 실사시 버튼 색상
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getStockAdjustColor(String stageCd) {
+		// cmm.ind.stock.adjustment.color
+		return null;
+	}
+	
+	/**
+	 * 표시기 버튼 기본 색상
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @return
+	 */
+	public static String getIndButtonDefaultColor(String stageCd) {
+		// cmm.ind.button.default.color
+		return null;
+	}
+	
+	/**
+	 * 스테이지 공통 - 바코드 최대 길이
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @param jobType 작업 유형
+	 * @return
+	 */
+	public static String getSkuBarcdMaxLength(String stageCd, String jobType) {
+		// cmm.sku.barcode.max.length
+		return null;
+	}
+	
+	/**
+	 * 스테이지 공통 - 상품 조회를 위한 코드 필드명 리스트
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @param jobType 작업 유형
+	 * @return
+	 */
+	public static String getSearchSkuFields(String stageCd, String jobType) {
+		// cmm.sku.search.code.fields
+		return null;
+	}
+	
+	/**
+	 * 스테이지 공통 - 주문 필드 중에 박스 처리시에 출고 분류 코드로 사용할 필드 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @param jobType 작업 유형
+	 * @return
+	 */
+	public static String getBoxOutClassCodeField(String stageCd, String jobType) {
+		// cmm.box.out.class.field
+		return null;
+	}
+	
+	/**
+	 * 스테이지 공통 - 기본 셀 매핑 대상 필드 명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @param jobType 작업 유형
+	 * @return
+	 */
+	public static String getCellMappingTargetField(String stageCd, String jobType) {
+		// cmm.cell.mapping.target.field
+		return null;
+	}
+	
+	/**
+	 * 스테이지 공통 - 표시기 유형 (통신 프로토콜 기준)
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @param jobType 작업 유형
+	 * @return
+	 */
+	public static String getIndicatorType(String stageCd, String jobType) {
+		// cmm.indicator.type
+		return null;
+	}
+	
+	/**
+	 * 스테이지 공통 - 주문 취소시 데이터 삭제 여부
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @param jobType 작업 유형
+	 * @return
+	 */
+	public static boolean isOrderDeleteWhenOrderCancel(String stageCd, String jobType) {
+		// cmm.order.delete.when.order_cancel
+		return false;
+	}
+	
+	/**
+	 * 스테이지 공통 - 주문 그룹 필드로 사용할 주문 테이블의 필드명
+	 * 
+	 * @param stageCd 스테이지 코드
+	 * @param jobType 작업 유형
+	 * @return
+	 */
+	public static String getOrderGroupField(String stageCd, String jobType) {
+		// cmm.order.ordergroup.field
+		return null;
+	}
+
 }
