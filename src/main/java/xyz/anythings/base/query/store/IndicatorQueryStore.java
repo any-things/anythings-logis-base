4747f500291d959c1c8d07698b81e3d2e1450b82
hatiolab <shortstop@hatiolab.com>
2019-11-14 18:33:46 +0900

    리팩토링 & GW 모듈 디펜던시 변경

--- src/main/java/xyz/anythings/base/query/store/IndicatorQueryStore.java
+++ src/main/java/xyz/anythings/base/query/store/IndicatorQueryStore.java
@@ -2,13 +2,57 @@ package xyz.anythings.base.query.store;
 
 import org.springframework.stereotype.Component;
 
+import xyz.elidom.sys.SysConstants;
+
 /**
- * 표시기 관련 쿼리 스토어
- * - 미들웨어, 게이트웨이, 표시기 관련 쿼리 관리
+ * 게이트웨이 쿼리 스토어
  * 
  * @author shortstop
  */
 @Component
 public class IndicatorQueryStore extends LogisBaseQueryStore {
+
+	@Override
+	public void initQueryStore(String databaseType) {
+		this.databaseType = databaseType;
+		this.basePath = "xyz/anythings/base/query/" + this.databaseType + SysConstants.SLASH;
+		this.defaultBasePath = "xyz/anythings/base/query/ansi/"; 
+	}
+	
+	/**
+	 * 게이트웨이 표시기 코드 리스트 쿼리
+	 * 
+	 * @return
+	 */
+	public String getIndCdListQuery() {
+		return this.getQueryByPath("ind/GatewayIndCdList");
+	}
+	
+	/**
+	 * 표시기 코드 리스트 조회 쿼리
+	 * 
+	 * @return
+	 */
+	public String getSearchIndicatorsQuery() {
+		return this.getQueryByPath("ind/SearchIndicators");
+	}
+
+	/**
+	 * 게이트웨이 소속 표시기를 사용하는 모든 설비 리스트를 조회
+	 *  
+	 * @return
+	 */
+	public String getEquipListByGateway() {
+		return this.getQueryByPath("ind/EquipListByGateway");
+	}
+
+	/**
+	 * 라우터 리부팅을 위한 표시기 리스트를 조회
+	 *  
+	 * @return
+	 */
+	public String getSearchIndListForGwInitQuery() {
+		return this.getQueryByPath("ind/SearchIndListForGwInit");
+	}
 
 }
