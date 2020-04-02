db722a6869a5ee3a8622a49128603624625dd767
hatiolab <shortstop@hatiolab.com>
2020-04-02 17:57:15 +0900

    배치 마감 이벤트 수정

--- src/main/java/xyz/anythings/base/event/main/BatchCloseEvent.java
+++ src/main/java/xyz/anythings/base/event/main/BatchCloseEvent.java
@@ -1,5 +1,7 @@
 package xyz.anythings.base.event.main;
 
+import xyz.anythings.base.entity.JobBatch;
+
 /**
  * 배치 작업 마감 이벤트
  * 
@@ -7,7 +9,7 @@ package xyz.anythings.base.event.main;
  */
 public class BatchCloseEvent extends BatchRootEvent {
 
-	public BatchCloseEvent(short eventStep) {
-		super(eventStep);
+	public BatchCloseEvent(JobBatch batch, short eventStep) {
+		super(batch, eventStep);
 	}
 }
