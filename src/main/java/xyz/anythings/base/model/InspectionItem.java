910b1717762ddf52228159ff25370f2a24af4b63
hatiolab <shortstop@hatiolab.com>
2020-06-01 10:11:47 +0900

    출고 검수 관련 모델

---
+++ src/main/java/xyz/anythings/base/model/InspectionItem.java
@@ -0,0 +1,10 @@
+package xyz.anythings.base.model;
+
+/**
+ * 검수 항목
+ * 
+ * @author shortstop
+ */
+public class InspectionItem {
+
+}
---
+++ src/main/java/xyz/anythings/base/model/OutInspection.java
@@ -0,0 +1,10 @@
+package xyz.anythings.base.model;
+
+/**
+ * 출고 검수 모델
+ * 
+ * @author shortstop
+ */
+public class OutInspection {
+
+}
