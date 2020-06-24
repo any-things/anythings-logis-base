b2d4d314d8de7c1b80f33ae8e4f472460463d868
jyp220 <jyp@hatiolab.com>
2020-06-24 21:18:02 +0900

    add class_cd

--- src/main/java/xyz/anythings/base/entity/Cell.java
+++ src/main/java/xyz/anythings/base/entity/Cell.java
@@ -69,6 +69,9 @@ public class Cell extends xyz.elidom.orm.entity.basic.ElidomStampHook {
 
 	@Column (name = "active_flag", length = 1)
 	private Boolean activeFlag;
+	
+	@Column (name = "class_cd", length = 30)
+	private String classCd;
   
 	public String getId() {
 		return id;
@@ -188,6 +191,14 @@ public class Cell extends xyz.elidom.orm.entity.basic.ElidomStampHook {
 
 	public void setActiveFlag(Boolean activeFlag) {
 		this.activeFlag = activeFlag;
+	}
+	  
+	public String getClassCd() {
+		return classCd;
+	}
+
+	public void setClassCd(String classCd) {
+		this.classCd = classCd;
 	}
 	
 	@Override
